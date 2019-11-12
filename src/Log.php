73073871d3e967c13af7214506bc7134d9f6e50f
AkioSarkiz <akiosarkiz@gmail.com>
2019-11-12 15:00:44 +0200

    update

--- src/Log.php
+++ src/Log.php
@@ -1,18 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 
 namespace Loger;
 
 
 use DateTime;
-use InvalidArgumentException;
 
 
+/**
+ * Class Log
+ * @package Loger
+ * @license MIT
+ */
 class Log
 {
     const INFO = 1;
     const ERROR = 2;
     const WARNING = 3;
+
+    private static $autoWrite = false;
 
     /** @var int */
     public $type;
@@ -24,17 +32,125 @@ class Log
     public $filename;
     /** @var DateTime */
     public $dateTime;
+    /** @var String */
+    private static $formatFile = '.log';
 
 
+    /**
+     * Log constructor.
+     * @param int $type
+     * @param String $title
+     * @param String $massage
+     * @param String $filename
+     */
     public function __construct(int $type, String $title, String $massage, String $filename)
     {
-        if (!($type == self::INFO || $type == self::ERROR || $type == self::WARNING))
-            throw new InvalidArgumentException('error', 785);
-
         $this->type = $type;
         $this->title = $title;
         $this->massage = $massage;
-        $this->filename = $filename;
-        $this->dateTime = new DateTime();
+        $this->filename = $filename . self::$formatFile;
+        try {
+            $this->dateTime = (new DateTime())->format('Y-m-d H:i:s');
+        }catch (\Exception $e) {
+            $this->dateTime = 'error';
+        }
+    }
+
+    /**
+     * История для записи в конце программы
+     * @var array
+     */
+    private static $logList = [];
+    private static $path = __DIR__;
+    public const DEFAULT_FILE_LOG = 'general.log';
+
+    /**
+     * @param String $title
+     * @param String $massage
+     * @param String $filename
+     * @return Void
+     */
+    public static function addInfo(String $title, String $massage = null, String $filename = self::DEFAULT_FILE_LOG): Void
+    {
+        array_push(self::$logList, new self(self::INFO, $title, $massage ?? '', $filename));
+        if (self::$autoWrite) self::writeToFile();
+    }
+
+    /**
+     * @param String $title
+     * @param String $massage
+     * @param String $filename
+     * @return Void
+     */
+    public static function addError(String $title, String $massage = null, String $filename = self::DEFAULT_FILE_LOG): Void
+    {
+        array_push(self::$logList, new self(self::ERROR, $title, $massage ?? '', $filename));
+        if (self::$autoWrite) self::writeToFile();
+    }
+
+    /**
+     * @param String $title
+     * @param String $massage
+     * @param String $filename
+     * @return Void
+     */
+    public static function addWarning(String $title, String $massage = null, String $filename = self::DEFAULT_FILE_LOG): Void
+    {
+        array_push(self::$logList, new self(self::WARNING, $title, $massage ?? '', $filename));
+        if (self::$autoWrite) self::writeToFile();
+    }
+
+    /**
+     * @return Void
+     */
+    public static function writeToFile(): Void
+    {
+        if (!file_exists(self::$path. '/log') || !is_dir(!self::$path . '/log'))
+            mkdir((self::$path . '/log'));
+
+        foreach (self::$logList as $log)
+        {
+            $handle = fopen(self::$path. '/log/' . $log->filename, 'a+');
+            $tag = null;
+            switch ($log->type){
+                case Log::INFO:
+                    $tag = 'i';
+                    break;
+                case Log::ERROR:
+                    $tag = 'e';
+                    break;
+                case Log::WARNING:
+                    $tag = 'w';
+                    break;
+            }
+
+            fwrite($handle, sprintf("%s [%s]\t%s\t%s" . PHP_EOL,
+                $tag,
+                $log->dateTime,
+                $log->title,
+                $log->massage
+            ));
+
+            fclose($handle);
+        }
+        self::$logList = [];
+    }
+
+    /**
+     * @param String $path
+     * @return Void
+     */
+    public static function setPathLog(String $path): Void
+    {
+        self::$path = $path;
+    }
+
+    /**
+     * @param bool $index
+     * @return Void
+     */
+    public static function setAutoWrite(bool $index): Void
+    {
+        self::$autoWrite = $index;
     }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit5d7f54c8bd0a7b7caa74f83bae341467
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -47,6 +50,24 @@ class ComposerAutoloaderInit5d7f54c8bd0a7b7caa74f83bae341467
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5d7f54c8bd0a7b7caa74f83bae341467::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5d7f54c8bd0a7b7caa74f83bae341467($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5d7f54c8bd0a7b7caa74f83bae341467($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
